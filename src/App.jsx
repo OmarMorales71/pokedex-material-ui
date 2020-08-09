b4452d266bf67b77f221ce034146bd66003bb4de
mancubus <mancubus@sourcedevil.com>
2020-08-08 20:05:08 -0500

    Implemented Router

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Pokedex from './Pokedex';
+import Pokemon from './Pokemon';
+
+
+function App() {
+  return (
+    <Switch>
+      <Route exact path="/" render={(props) => <Pokedex {...props} />}/>
+      <Route exact path="/:pokemonId" render={(props)=><Pokemon {...props} />} />
+    </Switch>
+  );
+}
+
+export default App;
---
+++ src/Pokedex.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Pokedex = ()=>{
+
+    return (
+        <div>This is the pokedex</div>
+    )
+}
+
+export default Pokedex
